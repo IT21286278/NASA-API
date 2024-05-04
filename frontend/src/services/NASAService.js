6636152cc6589a7994cbe574bb94f7feff7611db
Ashen_IT21286278 <99505510+IT21286278@users.noreply.github.com>
2024-05-05 04:24:04 +0530

    Refactor NASAService.js to use fetch instead of axios for getEarthImageryPhoto function

--- frontend/src/services/NASAService.js
+++ frontend/src/services/NASAService.js
@@ -1,33 +1,20 @@
-import axios from 'axios';
-
 const API_KEY = 'fHCMuT8DH6AsEOMpcITm7ZUGESMhuDVg54DoLxM8';
 const BaseURL = 'https://api.nasa.gov';
 
-const getMarstRoverPhotos = async (earth_date) => {
-  const response = await axios.get(
-    `${BaseURL}/mars-photos/api/v1/rovers/curiosity/photos`,
-    {
-      params: {
-        earth_date: earth_date,
-        api_key: API_KEY,
-      },
+const NASAService = {
+  getEarthImageryPhoto: async (lat, lon, date) => {
+    try {
+      const response = await fetch(
+        `${BaseURL}/planetary/earth/assets?lon=${lon}&lat=${lat}&date=${date}&dim=0.15&api_key=${API_KEY}`
+      );
+      const data = await response.json();
+      console.log('Earth Imagery :', data);
+      return data;
+    } catch (error) {
+      console.error('Error occured when fetching earth imagery photos:', error);
+      return {};
     }
-  );
-  return response.data.photos;
+  },
 };
 
-const getEarthImageryPhoto = async (lat, lon, date) => {
-  try {
-    const response = await fetch(
-      `${BaseURL}/planetary/earth/assets?lon=${lon}&lat=${lat}&date=${date}&dim=0.15&api_key=${API_KEY}`
-    );
-    const data = await response.json();
-    console.log('Earth Imagery :', data);
-    return data;
-  } catch (error) {
-    console.error('Error occured when fetching earth imagery photos:', error);
-    return {};
-  }
-};
-
-export default { getMarstRoverPhotos, getEarthImageryPhoto };
+export default NASAService;
