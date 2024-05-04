dee8bca7927619f7ee54eaa7f5f7e5ac5fcc5c9a
Ashen_IT21286278 <99505510+IT21286278@users.noreply.github.com>
2024-05-05 03:07:04 +0530

    Add ContentNotFound component

---
+++ frontend/src/components/ContentNotFound.jsx
@@ -0,0 +1,7 @@
+export default function ContentNotFound({ content }) {
+  return (
+    <div className="flex items-center justify-center h-[58vh] w-full ">
+      <h1 className="text-4xl font-bold text-gray-700">{content}</h1>
+    </div>
+  );
+}
