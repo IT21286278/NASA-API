6a4f0af81f204d85bf4f0f8cdf404b7b44c25523
Ashen_IT21286278 <99505510+IT21286278@users.noreply.github.com>
2024-05-05 02:58:50 +0530

    ImageModel Added

---
+++ frontend/src/components/ImageModel.jsx
@@ -0,0 +1,44 @@
+import Modal from 'react-modal';
+import { TransformWrapper, TransformComponent } from 'react-zoom-pan-pinch';
+
+export default function ImageModel({
+  src,
+  modalIsOpen,
+  closeModal,
+  full_name,
+}) {
+  return (
+    <Modal
+      isOpen={modalIsOpen}
+      onRequestClose={closeModal}
+      className="flex items-center justify-center h-full"
+      shouldCloseOnOverlayClick={true}
+      style={{
+        overlay: {
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+        },
+        content: {
+          position: 'relative',
+          inset: 'auto',
+        },
+      }}
+    >
+      <TransformWrapper>
+        <TransformComponent>
+          <img
+            src={src}
+            alt={full_name ? full_name : 'Image'}
+            style={{
+              maxWidth: '90vw',
+              maxHeight: '90vh',
+              objectFit: 'contain',
+              zIndex: 1000,
+            }}
+          />
+        </TransformComponent>
+      </TransformWrapper>
+    </Modal>
+  );
+}
