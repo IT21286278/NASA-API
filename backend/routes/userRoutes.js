aed8f6c60a27f343c600a8f01812e70b5157fcfc
Ashen_IT21286278 <99505510+IT21286278@users.noreply.github.com>
2024-05-05 13:36:36 +0530

    User Routes Implemented

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import { login, register } from '../controllers/userController.js';
+
+const router = express.Router();
+
+router.post('/register', register);
+
+router.post('/login', login);
+
+router.get('/me', async (req, res) => {
+  try {
+    return res.status(200).json({ ...req.user._doc });
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+});
+
+export default router;
