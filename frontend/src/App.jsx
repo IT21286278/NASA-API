2b4890749922324be6fb67306791ca7cc4d18164
Ashen_IT21286278 <99505510+IT21286278@users.noreply.github.com>
2024-05-05 14:47:59 +0530

    Change App.jsx with user management

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,26 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import LogInPage from './pages/LogInPage';
 import MarsRoversPage from './pages/MarsRoversPage';
 import HomePage from './pages/HomePage';
 import EarthImagery from './pages/EarthImagery';
 import PODPage from './pages/PODPage';
+import { ToastContextProvider } from './context/ToastContext';
+import { AuthContextProvider } from './context/AuthContext';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<LogInPage />} />
-        <Route path="/Marsrover" element={<MarsRoversPage />} />
-        <Route path="/Homepage" element={<HomePage />} />
-        <Route path="/EarthImagery" element={<EarthImagery />} />
-        <Route path="/picture-of-the-day" element={<PODPage />} />
-      </Routes>
-    </Router>
+    <ToastContextProvider>
+      <AuthContextProvider>
+        <Routes>
+          <Route path="/" element={<LogInPage />} />
+          <Route path="/Marsrover" element={<MarsRoversPage />} />
+          <Route path="/Homepage" element={<HomePage />} />
+          <Route path="/EarthImagery" element={<EarthImagery />} />
+          <Route path="/picture-of-the-day" element={<PODPage />} />
+        </Routes>
+      </AuthContextProvider>
+    </ToastContextProvider>
   );
 }
 
