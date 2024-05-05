44159c51b9215b361d2c9a323c7861bd5ae626cd
Ashen_IT21286278 <99505510+IT21286278@users.noreply.github.com>
2024-05-05 17:51:31 +0530

    Testing configuration files

--- frontend/src/components/Loading.jsx
+++ frontend/src/components/Loading.jsx
@@ -1,7 +1,9 @@
 export default function Loading() {
   return (
     <div className="flex items-center justify-center h-[70vh]">
-      <div className="animate-spin  rounded-full h-32 w-32 border-t-8 border-b-8 border-white"></div>
+      <div className="animate-spin  rounded-full h-32 w-32 border-t-8 border-b-8 border-white">
+        <span className="sr-only">Loading...</span>
+      </div>
     </div>
   );
 }
